b1ab7bfd7cbac48c44da59eed8906ce82e9487c3
admin <nivyaxavier@gmail.com>
2024-12-18 23:58:07 +0530

    Add project files.

---
+++ Sample/Program.cs
@@ -0,0 +1,21 @@
+﻿class Person
+{
+    public string Name;
+    public Person(string name)
+    {
+        Name = name;
+    }
+    public void Display()
+    {
+        Console.WriteLine($"Name is {Name}");
+    }
+}
+class Program
+{
+    static void Main(string[] args)
+    {
+        Person p = new Person("Unknown");
+        p.Display();
+        Console.ReadKey();
+    }
+}
